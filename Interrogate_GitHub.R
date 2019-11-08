ddf6cd4d57ea194d90c93175e4567fdda0a78adb
Oran McVeigh Keeling <mcveigho@ITSERV.SCSS.TCD.IE>
2019-11-08 10:11:39 +0000

    Adaptations to sourced code

--- Interrogate_GitHub.R
+++ Interrogate_GitHub.R
@@ -20,7 +20,7 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+req <- GET("https://api.github.com/users/okeeling/repos", gtoken)
 
 # Take action on http error
 stop_for_status(req)
@@ -32,4 +32,4 @@ json1 = content(req)
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+gitDF[gitDF$full_name == "okeeling/datasharing", "created_at"] 
